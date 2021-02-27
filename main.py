b551480af438e983476f9a8c8daebecd6958dc8f
Vyzytor <59753939+Vyzytor@users.noreply.github.com>
2021-02-27 12:00:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,24 @@ app = Flask(__name__)
 #  val = {"value": value}
 #  return jsonify(val)
 
-@app.route('/')
-def get_days():
-  """Return a friendly HTTP greeting."""
-  num=input('how many days: ')
-  val = {"value": num}
-  return jsonify(val)
+#@app.route('/')
+#def get_days():
+#  """Return a friendly HTTP greeting."""
+#  num=input('how many days: ')
+#  val = {"value": num}
+#  return jsonify(val)
+
+@app.route("/")
+def homepage():
+    return render_template("page.html", title="HOME PAGE")
+
+@app.route("/docs")
+def docs():
+    return render_template("page.html", title="docs page")
+
+@app.route("/about")
+def about():
+    return render_template("page.html", title="about page")
   
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
