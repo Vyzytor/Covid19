8621b135bcc5ffd5ca4bbfd58450e6d2973ac9b0
Vyzytor <59753939+Vyzytor@users.noreply.github.com>
2021-03-13 10:59:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,129 @@
 from flask import Flask, render_template, request
 from flask import jsonify
+import json
+import html
 import google.auth
 from google.cloud import bigquery
-#from google.cloud import bigquery_storage
+from google.cloud import storage
 from google.oauth2 import service_account
-#credentials = service_account.Credentials.from_service_account_file(
-#'path/to/file.json')
-#project_id = 'covid19-302022'
 
 
-credentials, project_id = google.auth.default(
-    scopes=["https://www.googleapis.com/auth/cloud-platform"]
-)
+import argparse
+import googleapiclient.discovery
 
-bqclient = bigquery.Client(credentials= credentials,project=project_id)
-#bqstorageclient = bigquery_storage.BigQueryReadClient(credentials=credentials)
-    
+
 app = Flask(__name__)
+@app.route('/')
+def predict():
+    
 
-#@app.route('/')
-#def hello():
-#  """Return a friendly HTTP greeting."""
-#  val = {"value": "Hello World!*!*!*"}
-#  return jsonify(val)
 
-#@app.route('/name/<value>')
-#def name(value):
-#  val = {"value": value}
-#  return jsonify(val)
+    credentials, project_id = google.auth.default(
+        scopes=["https://www.googleapis.com/auth/cloud-platform"]
+    )
+    project_id = 'covid19-302022'
+    bqclient = bigquery.Client(credentials= credentials,project=project_id)
 
-#@app.route('/')
-#def get_days():
-#  """Return a friendly HTTP greeting."""
-#  num=input('how many days: ')
-#  val = {"value": num}
-#  return jsonify(val)
+    #get predicted cases
+    query_string= """
+    SELECT
+    DATE(forecast_timestamp) as PredDate, round(m.forecast_value) as Cases
+    FROM
+    ML.FORECAST(MODEL Covid19NYT.cases_arima_model,
+                STRUCT(30 AS horizon, 0.8 AS confidence_level)) m
+    where m.forecast_timestamp = (select max(forecast_timestamp)
+    FROM
+    ML.FORECAST(MODEL Covid19NYT.cases_arima_model,
+                STRUCT(30 AS horizon, 0.8 AS confidence_level))
+    )
+    """
+    query_job = bqclient.query(query_string)
+    results = query_job.result()
+    for row in results:
+        CasesPredictionDate = row.PredDate
+        PredCases = row.Cases
 
-@app.route("/")
-def homepage():
-    return render_template("page.html", title="HOME PAGE")
+        
+    #get predicted deaths
+    query_string= """
+    SELECT
+    DATE(forecast_timestamp) as PredDate, round(m.forecast_value) as Deaths
+    FROM
+    ML.FORECAST(MODEL Covid19NYT.deaths_arima_model,
+                STRUCT(30 AS horizon, 0.8 AS confidence_level)) m
+    where m.forecast_timestamp = (select max(forecast_timestamp)
+    FROM
+    ML.FORECAST(MODEL Covid19NYT.deaths_arima_model,
+                STRUCT(30 AS horizon, 0.8 AS confidence_level))
+    )
+    """
+    query_job = bqclient.query(query_string)
+    results = query_job.result()
+    for row in results:
+        DeathsPredictionDate = row.PredDate
+        PredDeaths = row.Deaths
 
-@app.route("/predict")
-def docs():
-    return render_template("page.html", title="predict page")
+    #get most recent deaths
+    query_string= """
+    SELECT
+    cast(deaths_date_history as TIMESTAMP) AS deaths_date_history,
+    total_deaths_history as deaths_history_value,
 
-@app.route("/about")
-def about():
-    return render_template("page.html", title="about page")
+    FROM
+    (
+    SELECT
+    date as deaths_date_history,
+    SUM(daily_deaths) AS total_deaths_history
+    FROM
+    `covid19-302022.Covid19NYT.DailyCasesDeaths`
+    GROUP BY date
+    ORDER BY date ASC
+    ) dcd
+    where dcd.deaths_date_history = (select max(date)
+    FROM `covid19-302022.Covid19NYT.DailyCasesDeaths`)
+    """
+    query_job = bqclient.query(query_string)
+    results = query_job.result()
+    for row in results:
+        DeathsDate = row.deaths_date_history
+        QtyDeaths = row.deaths_history_value
+
+    #get most recent cases
+    query_string= """
+    SELECT
+    cast(cases_date_history as TIMESTAMP) AS cases_date_history,
+    total_cases_history as cases_history_value,
+
+    FROM
+    (
+    SELECT
+    date as cases_date_history,
+    SUM(daily_confirmed_cases) AS total_cases_history
+    FROM
+    `covid19-302022.Covid19NYT.DailyCasesDeaths`
+    GROUP BY date
+    ORDER BY date ASC
+    ) dcd
+    where dcd.cases_date_history = (select max(date)
+    FROM `covid19-302022.Covid19NYT.DailyCasesDeaths`)
+    """
+    query_job = bqclient.query(query_string)
+    results = query_job.result()
+    for row in results:
+        CasesDate = row.cases_date_history
+        QtyCases = row.cases_history_value
+
+    val = {"CasesPredictionDate":CasesPredictionDate.strftime('%m/%d/%Y'), "PredictedCases":PredCases,
+            "DeathsPredictionDate":DeathsPredictionDate.strftime('%m/%d/%Y'),"PredictedDeaths":PredDeaths,
+            "DeathsDate":DeathsDate.strftime('%m/%d/%Y'),"MostRecentDeathCount":QtyDeaths,
+            "CasesDate":CasesDate.strftime('%m/%d/%Y'),"MostRecentCasesCount":QtyCases}
+
+    return render_template("page.html", title="Covid19: Current Stats and Predictions",jsonfile = val )
+
   
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8080, debug=True)
 
-# add a note 888
-
+    #predict()
+ 
+    #app.run(host='127.0.0.1', port=8080, debug = True)
+    app.run(host='0.0.0.0')
